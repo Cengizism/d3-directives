94540e44631107f2251a77a9bee4d26cb07a3332
Cengizism <culusoy@ask-cs.com>
2014-03-03 14:06:29 +0100

    cost diagram with preventive and corrective values

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,126 +2,59 @@
 
 angular.module('d3StackedBarsApp')
   .controller('MainCtrl', function ($scope) {
-    
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+
+    $scope.stackedBarsData = [
+      {
+        year: 13,
+        preventive: 100,
+        corrective: 12
+      },
+      {
+        year: 14,
+        preventive: 80,
+        corrective: 22
+      },
+      {
+        year: 15,
+        preventive: 110,
+        corrective: 30
+      },
+      {
+        year: 16,
+        preventive: 150,
+        corrective: 40
+      },
+      {
+        year: 17,
+        preventive: 80,
+        corrective: 12
+      },
+      {
+        year: 18,
+        preventive: 120,
+        corrective: 40
+      },
+      {
+        year: 19,
+        preventive: 80,
+        corrective: 60
+      },
+      {
+        year: 20,
+        preventive: 110,
+        corrective: 42
+      },
+      {
+        year: 21,
+        preventive: 100,
+        corrective: 30
+      },
+      {
+        year: 22,
+        preventive: 100,
+        corrective: 10
+      }
     ];
-    
-    
-    // console.log('=>', d3);
-    
-    
-    $scope.greeting = "Resize the page to see the re-rendering";
-    $scope.data = [
-      {name: "Greg", score: 98},
-      {name: "Ari", score: 96},
-      {name: 'Q', score: 75},
-      {name: "Loser", score: 48}
-    ];
-//    
-//    
-//    var margin  = {top: 20, right: 20, bottom: 30, left: 40},
-//        width   = 960 - margin.left - margin.right,
-//        height  = 500 - margin.top - margin.bottom;
-//
-//    var x = d3.scale.ordinal()
-//              .rangeRoundBands([0, width], .1);
-//
-//    var y = d3.scale.linear()
-//              .rangeRound([height, 0]);
-//
-//    var color = d3.scale.ordinal()
-//                  .range([
-//                    "#98abc5", 
-//                    "#8a89a6", 
-//                    "#7b6888", 
-//                    "#6b486b", 
-//                    "#a05d56", 
-//                    "#d0743c", 
-//                    "#ff8c00"]);
-//
-//    var xAxis = d3.svg.axis()
-//                  .scale(x)
-//                  .orient("bottom");
-//
-//    var yAxis = d3.svg.axis()
-//                  .scale(y)
-//                  .orient("left")
-//                  .tickFormat(d3.format(".2s"));
-//
-//    var svg = d3.select("body").append("svg")
-//                .attr("width", width + margin.left + margin.right)
-//                .attr("height", height + margin.top + margin.bottom)
-//                .append("g")
-//                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//
-//    d3.csv("data/data-stacked-bars.csv", function(error, data) {
-//      color.domain(d3.keys(data[0]).filter(function(key) { return key !== "State"; }));
-//
-//      data.forEach(function(d) {
-//        var y0 = 0;
-//        d.ages = color
-//                  .domain()
-//                  .map(function(name) { return {name: name, y0: y0, y1: y0 += +d[name]}; });
-//        d.total = d.ages[d.ages.length - 1].y1;
-//      });
-//
-//      data.sort(function(a, b) { return b.total - a.total; });
-//
-//      x.domain(data.map(function(d) { return d.State; }));
-//      y.domain([0, d3.max(data, function(d) { return d.total; })]);
-//
-//      svg.append("g")
-//          .attr("class", "x axis")
-//          .attr("transform", "translate(0," + height + ")")
-//          .call(xAxis);
-//
-//      svg.append("g")
-//          .attr("class", "y axis")
-//          .call(yAxis)
-//          .append("text")
-//          .attr("transform", "rotate(-90)")
-//          .attr("y", 6)
-//          .attr("dy", ".71em")
-//          .style("text-anchor", "end")
-//          .text("Population");
-//
-//      var state = svg.selectAll(".state")
-//          .data(data)
-//          .enter().append("g")
-//          .attr("class", "g")
-//          .attr("transform", function(d) { return "translate(" + x(d.State) + ",0)"; });
-//
-//      state.selectAll("rect")
-//          .data(function(d) { return d.ages; })
-//          .enter().append("rect")
-//          .attr("width", x.rangeBand())
-//          .attr("y", function(d) { return y(d.y1); })
-//          .attr("height", function(d) { return y(d.y0) - y(d.y1); })
-//          .style("fill", function(d) { return color(d.name); });
-//
-//      var legend = svg.selectAll(".legend")
-//          .data(color.domain().slice().reverse())
-//          .enter().append("g")
-//          .attr("class", "legend")
-//          .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-//
-//      legend.append("rect")
-//          .attr("x", width - 18)
-//          .attr("width", 18)
-//          .attr("height", 18)
-//          .style("fill", color);
-//
-//      legend.append("text")
-//          .attr("x", width - 24)
-//          .attr("y", 9)
-//          .attr("dy", ".35em")
-//          .style("text-anchor", "end")
-//          .text(function(d) { return d; });
-//
-//    });
     
     
     
--- app/scripts/directives/d3-stacked-bars.js
+++ app/scripts/directives/d3-stacked-bars.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('d3StackedBarsApp.directives', [])
-.directive('d3Bars',
+.directive('d3StackedBars',
       [
         '$window', '$timeout',
         function ($window, $timeout)
@@ -13,114 +13,176 @@ angular.module('d3StackedBarsApp.directives', [])
               label:    '@',
               onClick:  '&'
             },
-            link: function(scope, ele, attrs) {
+            link: function (scope, ele, attrs)
+            {
+              var renderTimeout;            
               
-              console.log('coming to here?');
+              var margin  = {top: 20, right: 20, bottom: 30, left: 40},
+                  width   = 960 - margin.left - margin.right,
+                  height  = 500 - margin.top - margin.bottom;
 
-              var renderTimeout,
-                  margin      = parseInt(attrs.margin) || 20,
-                  barHeight   = parseInt(attrs.barHeight) || 20,
-                  barPadding  = parseInt(attrs.barPadding) || 5;
+              var data = scope.data;
 
-              var svg = d3.select(ele[0])
-                .append('svg')
-                .style('width', '100%');
+              var x = d3.scale.ordinal()
+                        .rangeRoundBands([0, width], .1);
 
-              $window.onresize = function ()
-              {
-                scope.$apply();
-              };
+              var y = d3.scale.linear()
+                        .rangeRound([height, 0]);
 
-              scope.$watch(
-                function ()
+              var color = d3.scale.ordinal()
+                            .range(["#98abc5", "#8a89a6"]);
+
+              var xAxis = d3.svg.axis()
+                            .scale(x)
+                            .orient("bottom");
+
+              var yAxis = d3.svg.axis()
+                            .scale(y)
+                            .orient("left")
+                            .tickFormat(d3.format(".2s"));
+
+              var svg = d3.select("body").append("svg")
+                          .attr("width", width + margin.left + margin.right)
+                          .attr("height", height + margin.top + margin.bottom)
+                          .append("g")
+                          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+              color.domain(d3.keys(data[0])
+                .filter(function(key) { return key !== "year"; }));
+
+              data.forEach(
+                function (d)
                 {
-                  return angular.element($window)[0].innerWidth;
-                },
-                function ()
-                {
-                  scope.render(scope.data);
+                  var y0 = 0;
+
+                  d.ages = color
+                    .domain()
+                    .map(function (name)
+                    {
+                      return {name: name, y0: y0, y1: y0 += +d[name]};
+                    });
+
+                  d.total = d.ages[d.ages.length - 1].y1;
                 }
               );
 
-              scope.$watch('data', function (newData)
-              {
-                scope.render(newData);
-              }, true);
+              // data.sort(function (a, b) { return b.total - a.total; });
 
-              scope.render = function (data)
-              {
-                svg.selectAll('*').remove();
+              x.domain(data.map(function (d) { return d.year; }));
+              y.domain([0, d3.max(data, function (d) { return d.total; })]);
 
-                if (!data) return;
-                if (renderTimeout) clearTimeout(renderTimeout);
+              svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
 
-                renderTimeout = $timeout(function ()
+              svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text("Costs");
+
+              var year = svg.selectAll(".state")
+                .data(data)
+                .enter().append("g")
+                .attr("class", "g")
+                .attr("transform",
+                function (d)
                 {
-                  var width   = d3.select(ele[0])[0][0].offsetWidth - margin,
-                      height  = scope.data.length * (barHeight + barPadding),
-                      color   = d3.scale.category20(),
-                      xScale  = d3.scale.linear()
-                                .domain([0, d3.max(data, function (d)
-                                  {
-                                    return d.score;
-                                  }
-                                )])
-                                .range([0, width]);
+                  return "translate(" + x(d.year) + ",0)";
+                });
 
-                  svg.attr('height', height);
-
-                  svg.selectAll('rect')
-                    .data(data)
-                    .enter()
-                    .append('rect')
-                    .on('click', function (d, i)
-                      {
-                        return scope.onClick({item: d});
-                      }
-                    )
-                    .attr('height', barHeight)
-                    .attr('width', 140)
-                    .attr('x', Math.round(margin/2))
-                    .attr('y', function (d, i)
-                      {
-                        return i * (barHeight + barPadding);
-                      }
-                    )
-                    .attr('fill', function (d)
-                      {
-                        return color(d.score);
-                      }
-                    )
-                    .transition()
-                    .duration(1000)
-                    .attr('width', function (d)
-                      {
-                        return xScale(d.score);
-                      }
-                    );
-
-                  svg.selectAll('text')
-                    .data(data)
-                    .enter()
-                    .append('text')
-                    .attr('fill', '#fff')
-                    .attr('y', function (d,i)
-                      {
-                        return i * (barHeight + barPadding) + 15;
-                      }
-                    )
-                    .attr('x', 15)
-                    .text(function (d)
-                      {
-                        return d.name + " (scored: " + d.score + ")";
-                      }
-                    );
-                }, 200);
+              year.selectAll("rect")
+                .data(function (d) { return d.ages; })
+                .enter().append("rect")
+                .attr("width", x.rangeBand())
+                .attr("y",
+                function (d)
+                {
+                  return y(d.y1);
+                })
+                .attr("height", function (d)
+                {
+                  return y(d.y0) - y(d.y1);
+                })
+                .style("fill", function (d) { return color(d.name); });
 
 
+              var legend = svg.selectAll(".legend")
+                .data(color.domain().slice().reverse())
+                .enter().append("g")
+                .attr("class", "legend")
+                .attr("transform",
+                function (d, i)
+                {
+                  return "translate(0," + i * 20 + ")";
+                });
 
+              legend.append("rect")
+                .attr("x", width - 18)
+                .attr("width", 18)
+                .attr("height", 18)
+                .style("fill", color);
 
-              };
+              legend.append("text")
+                .attr("x", width - 24)
+                .attr("y", 9)
+                .attr("dy", ".35em")
+                .style("text-anchor", "end")
+                .text(function (d) { return d; });
+              
+              
+              
+              
+//              $window.onresize = function () { scope.$apply(); };
+//
+//
+//
+//              scope.$watch(
+//                function () { return angular.element($window)[0].innerWidth; },
+//                function () { scope.render(scope.data); }
+//              );
+//
+//
+//              scope.$watch('data', function (newData) { scope.render(newData) }, true);
+//
+//
+//
+//              scope.render = function (data)
+//              {
+//
+//                // svg.selectAll('*').remove();
+//
+//                if (!data) return;
+//                if (renderTimeout) clearTimeout(renderTimeout);
+//
+//
+//
+//                renderTimeout = $timeout(function ()
+//                {
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//                }, 200);
+//
+//
+//
+//
+//              };
+              
+              
+              
+              
 
             }}
         }
